a347c7824216b5585fb774dd4b75bbb4bc3b20a8
Anna <kochurovaav@gmail.com>
2023-05-24 16:01:35 +0300

    Initial commit HW7 is done

---
+++ test.py
@@ -0,0 +1,26 @@
+# string = "пара-ра-рам рам-пам-папам па-ра-па-да рам-пам-папам"
+# main_list = string.split()
+# print(main_list)
+# glas = 'а'
+# list2 = []
+# b = 0
+#
+# for x in main_list:
+#     b = x.count(glas)
+#     list2.append(b)
+# print(list2)
+#
+# # list2 = list(map(lambda x: b == x.count(glas), main_list))
+# #
+# # list2.append(b)
+# # print(list2)
+#
+# for i in range(0, len(list2) -1 ):
+#     if list2[i] == list2[i + 1]:
+#      print('yes')
+#     else:
+#         print('no')
+#
+
+
+
