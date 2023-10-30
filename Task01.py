47947669459dbff36a4a90b60b0b20f5921456be
Anna <kochurovaav@gmail.com>
2023-10-31 01:18:20 +0200

    All tasks are modified

--- Task01.py
+++ Task01.py
@@ -8,16 +8,55 @@
 # Ввод:** пара-ра-рам рам-пам-папам па-ра-па-да
 #     **Вывод:** Парам пам-пам
 
+# Вариант 1:
 poem = "пара-ра-рам рам-пам-папам па-ра-па-да рам-пам-папам"
-lines = poem.split()
-
+lines = poem.split('-')
 
 list1 = [sum(x in 'уеыаоэяию' for x in line) for line in lines]
 
-if len(set(list1)) == 1:
+if len(list1) % 2 == 0:
     res = "Парам пам-пам"
 else:
     res = "Пам парам"
 
 print(res)
 
+
+
+# Вариант 2:
+#
+# stroka = 'пара-ра-рам рам-пам-папам па-ра-па-дам'
+# def rhythm(stroka):
+#     stroka = stroka.split()
+#     list = []
+#     for word in stroka:
+#         result = 0
+#         for i in word:
+#             if i in 'аеёиоуыэюя':
+#                 result += 1
+#         list.append(result)
+#     return len(list) == list.count(list[0])
+#
+# print('Введите: пара-ра-рам рам-пам-папам па-ра-па-дам')
+# str = input()
+# if rhythm(stroka):
+#     print('Парам пам-пам')
+# else:
+#     print('Пам парам')
+
+# Вариант 3:
+stroka = 'пара-ра-рам рам-пам-папам па-ра-па-дам'
+vowels = ['а', 'е', 'ё', 'и', 'й', 'о', 'у', 'ы', 'э', 'ю', 'я']
+phrases = stroka.split()
+if len(phrases) < 2:
+ print('Количество фраз должно быть больше одной!')
+else:
+ countVowels = []
+
+ for i in phrases:
+  countVowels.append(len([x for x in i if x.lower() in vowels]))
+
+ if countVowels.count(countVowels[0]) == len(countVowels):
+  print('Парам пам-пам')
+ else:
+  print('Пам парам')
--- Task02.py
+++ Task02.py
@@ -14,13 +14,34 @@
 # 5 10 15 20 25 30
 # 6 12 18 24 30 36
 
-
-def print_operation_table(operation, num_rows=6, num_columns=6):
+# Вариант 1:
+def print_operation_table(operation, num_rows, num_columns):
     a = [[operation(i, j)
           for j in range(1, num_columns + 1)]
          for i in range(1, num_rows + 1)]
+
     for i in a:
-        print(*[f"{x:>3}" for x in i])
+        if num_rows < 2:
+            print("ОШИБКА! Размерности таблицы должны быть больше 2!")
+        else:
+
+            print(*[f"{x:>3}" for x in i])
 
 
-print_operation_table(lambda x, y: x * y)
+print_operation_table(lambda x, y: x * y, 3, 3)
+
+
+# Вариант 2:
+
+def print_operation_table(operation, num_rows, num_columns):
+    if num_rows < 2 or num_columns < 2:
+        print('ОШИБКА! Размерности таблицы должны быть больше 2!')
+    else:
+        header = ' '.join([str(i) for i in range(1, num_columns + 1)])
+        print(header)
+        for i in range(2, num_rows + 1):
+            row = [str(i)] + [str(operation(i, j)) for j in range(2, num_columns + 1)]
+            print(' '.join(row))
+
+
+print_operation_table(lambda x, y: x * y, 3, 3)
